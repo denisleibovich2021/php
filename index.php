b0d09402efcd7a7e8b1f2cec2983916572d7a012
Denis leibovich <leibovich2021@gmail.com>
2022-07-15 14:58:21 +0300

    v3

--- index.php
+++ index.php
@@ -24,9 +24,7 @@ else
 <body>
     <section class="congratulations">
         <h1>hellooo Denisssssss!</h1>
-        <p>yourrrrr thee bestttt<em>PHP</em> application is now running on your own dedicated environment in the AWS&nbsp;Cloud</p>
-        <p>You are running PHP version <?= phpversion() ?></p>
-        <p>This environment is launched with Elastic Beanstalk PHP Platform</p>
+        <p>yourrrrr thee bestttt</p>
     </section>
 
     <section class="instructions">
